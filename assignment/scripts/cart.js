066f23aa5c908c75186e7d7d5f05cac233064c0c
Shong Lee <Shonglee17@gmail.com>
2022-11-18 15:31:25 -0600

    working on stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,32 +1,41 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
+let basket = []
 
-function addItem(a, b , c){
-    console.log('add these items into basket:', a , b , c )
-    basket.push()
-    return true;
+function addItem(...x){
+    console.log('add these items into basket:', x )
+    basket.push(...x )
+    return basket;
 }//end addItem function
-console.log('added items to basket:', addItem('a', 'b', 'c', 'd', 'e', 'f') )
+console.log('added items to basket:', addItem('a', 'b', 'c', 'd', 'e', 'f' ) )
 
 
-function listItems(x){
-    let z = x.forEach(printItem)
-    function printItem(x){
-        return x;   
-    }//end function print
-    return z;
+function listItems(basket){
+    for (i=0 ; i<basket.length ; i++){
+    console.log(basket[i])
+    }// end for
+    return basket;
+    
 }//end listItems
+console.log('list these items:', listItems(basket) ) 
 
-console.log(basket) 
-
-function empty(x){
-    console.log('in empty')
-    x.splice(0,10)
-    return x;
+function empty(basket){
+    console.log('are we in this')
+    for (i=0; i<basket.length ; i++){
+    basket.shift()
+    }
+    return basket;
 }//end empty
-console.log('array will be empty if this function is used!', )
+console.log('empty array with this function:', empty(basket))
+console.log(basket)
+function removeItem(basket){
+    console.log('in removeItem')
+    let index = basket.indexOf('a')
+    return index;
+}
+console.log('search this array:', basket)
+
 
 
 
